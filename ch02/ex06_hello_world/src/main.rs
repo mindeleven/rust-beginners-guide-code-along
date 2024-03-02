9c6bb849972345680efed0b32cfb783aa0381014
mindeleven <mindeleven@posteo.de>
2024-03-02 10:51:04 +0100

    A first hello world example

---
+++ ch02/ex06_hello_world/src/main.rs
@@ -0,0 +1,9 @@
+/// Coding along with the Udemy course 
+/// Rust lang: The complete beginner's guide by Catalin Stefan
+/// https://www.udemy.com/course/rustaceans
+/// Source code and comments take from the videos of this course
+
+fn main() {
+    println!("Hello, world!");
+    println!("A first Rust example from the Rust lang: The complete beginner's guide course");
+}
